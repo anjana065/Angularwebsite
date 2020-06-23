1a70f265175978d96fa1da6627256922df7431c0
anjanapadmesh <anjana.065@gmail.com>
2020-06-23 14:46:59 +0530

    first commit

---
+++ Scripts/app.js
@@ -0,0 +1,38 @@
+﻿/**
+ *
+ * Responsive website using AngularJS
+ * http://www.script-tutorials.com/responsive-website-using-angularjs/
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ * Copyright 2013, Script Tutorials
+ * http://www.script-tutorials.com/
+ */
+
+'use strict';
+
+// angular.js main app initialization
+var app = angular.module("myApp", []).
+    config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.
+            when('/', { templateUrl: 'pages/index.html', activetab: 'projects', controller: HomeCtrl }).
+            when('/project/:projectId', {
+                templateUrl: function (params) { return 'pages/' + params.projectId + '.html'; },
+                controller: ProjectCtrl,
+                activetab: 'projects'
+            }).
+            when('/privacy', {
+                templateUrl: 'pages/privacy.html',
+                controller: PrivacyCtrl,
+                activetab: 'privacy'
+            }).
+            when('/about', {
+                templateUrl: 'pages/about.html',
+                controller: AboutCtrl,
+                activetab: 'about'
+            });
+
+
+}]);
+
